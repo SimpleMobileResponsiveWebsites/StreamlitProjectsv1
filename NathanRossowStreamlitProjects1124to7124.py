b8c278f1fcf732925931d7e9e93f823576015cc2
Nathan Rossow <nathanrossow@gmail.com>
2024-07-08 11:14:32 -0500

    Create NathanRossowStreamlitProjects1124to7124.py

---
+++ NathanRossowStreamlitProjects1124to7124.py
@@ -0,0 +1,141 @@
+import streamlit as st
+
+# Personal Information
+st.title("Nathan Rossow")
+st.subheader("Python Application Developer, Regression Tester, Application Deployment Specialist, Project Manager")
+st.write("Email: simplemobileresponsivewebsites@gmail.com")
+st.write("LinkedIn: [linkedin.com/in/nathan-rossow-58912238](https://www.linkedin.com/in/nathan-rossow-58912238/)")
+st.write("GitHub: [github.com/SimpleMobileResponsiveWebsites](https://github.com/SimpleMobileResponsiveWebsites)")
+
+# Projects
+st.header("Projects")
+
+st.subheader("VCSToPDF")
+st.write("""
+- Developed a streamlit web application for  regression testing applications.
+- Deployed the application using streamlit cloud.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/BurstProjectManagementSoftware/blob/main/VCSToPDF.py)
+- [Go To App](https://vcstopdf.streamlit.app/)
+""")
+
+st.subheader("Supply Chain Analytics for Audio Equipment")
+st.write("""
+- Developed a comprehensive supply-chain analytics web application for audio equipment using python and streamlit.
+- Analyzed supply chain data to product pricing, product name, and product quantity.
+- Created interactive dashboards to visualize supply pricing, quantity metrics that drive
+data-driven business strategies and operations.
+- Deployed the solution using Streamlit Cloud.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/Supply-Chain-Analytics-For-Audio-Equipment)
+- [Go To App](https://supply-chain-analytics-for-audio-equipmentv3.streamlit.app/)
+""")
+
+st.subheader("CNC Manufacturing v1")
+st.write("""
+- Developed a python web application for CNC manufacturing that allows users to input various data fields related to client and job details, then save and download this information as both a CSV file and a PDF deployed with Streamlit.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/CNCManufacturingv1)
+- [Go To App](https://cncmanufacturingv1.streamlit.app/)
+""")
+
+st.subheader("Flooring Cost Estimator v1")
+st.write("""
+- Developed a python web application for a flooring cost estimator that allows users to input customer details, project dates, area dimensions, material and labor costs, and additional expenses. It calculates total costs and enables users to save and export this data to PDF or CSV formats.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/FlooringCostEstimatorv1)
+- [Go To App](https://flooringcostestimatorv1.streamlit.app/)
+""")
+
+st.subheader("Read It v1")
+st.write("""
+- Developed a streamlit web application named "ReadItV1" that allows users to upload a text file, view its contents, and generate audio playback of the text using Google Text-to-Speech (gTTS). 
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/ReadItV1)
+- [Go To App](https://readitv1.streamlit.app/)
+""")
+
+st.subheader("Music Track Info v1")
+st.write("""
+- Developed a streamlit web application called "Track Information Manager" that allows users to input track details and generate a PDF report with the provided information.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/MusicTrackInfov1/blob/main/MusicTrackInfov1.py)
+- [Go To App](https://musictrackinfov1.streamlit.app/)
+""")
+
+st.subheader("Streamlit Widgets v1")
+st.write("""
+- Developed and designed a streamlit web application to show clients what streamlit widgets are available to them for their application during the design process. 
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/StreamlitWidgetsV1)
+- [Go To App](https://vcstopdfv3.streamlit.app/)
+""")
+
+st.subheader("Burst Project Management Software v3")
+st.write("""
+- Developed a streamlit web application for managing and documenting application development details, with features to input, display, and export these details as a PDF.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/BurstProjectManagementSoftwarev3)
+- [Go To App](https://vcstopdfv3.streamlit.app/)
+""")
+
+st.subheader("PMDD Tracker v1")
+st.write("""
+- Developed a streamlit web application for tracking and visualizing symptom severity ratings. 
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/PMDDTrackerv1)
+- [Go To App](https://pmddtrackerv1.streamlit.app/)
+""")
+
+st.subheader("Bible Study Reference Tool")
+st.write("""
+- Developed a Streamlit web application called "Bible Study Reference Tool."  This application allows users to input references and notes for Bible study, formats the inputted information, and generates a downloadable PDF containing the details and notes.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/BibleStudyReferenceTool)
+- [Go To App](https://biblestudyreferencetool.streamlit.app/)
+""")
+
+st.subheader("Burst Project Management Software v2")
+st.write("""
+- Developed a streamlit web application designed to collect and manage information about different versions of an app.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/BurstProjectManagementSoftwarev2)
+- [Go To App](https://vcstopdfv2.streamlit.app/)
+""")
+
+st.subheader("Tasks In JSON Array To Dashboard")
+st.write("""
+- Developed a streamlit application that allows users to input a JSON array, convert it into a DataFrame, and download the DataFrame as a CSV file.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/TasksInJSONArrayToDashboard)
+- [Go To App](https://tasksinjsonarraytodashboardv1.streamlit.app/)
+""")
+
+st.subheader("Time Management Tools To JSON v1")
+st.write("""
+- Developed a streamlit web application designed for managing tasks and time effectively.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/TimeMangementTools/blob/main/TimeManagementToolsToJSONv1.py)
+- [Go To App](https://timemanagementtoolstojsonv1.streamlit.app/)
+""")
+
+st.subheader("Google Ads Spend Visualization")
+st.write("""
+- Developed a streamlit web application for visualizing Google Ads spend data.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/GoogleAdsSpendVisualization/blob/main/GoogleAdsSpendVisualizationv1.py)
+- [Go To App](https://digitalrecipe.streamlit.app/)
+""")
+
+st.subheader("Digital Recipe")
+st.write("""
+- Developed a streamlit web application for creating, managing, and downloading recipes.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/DigitalRecipe)
+- [Go To App](https://digitalrecipe.streamlit.app/)
+""")
+
+st.subheader("Audio Agenda")
+st.write("""
+- Developed a streamlit web application named "Audio Agenda" for managing and visualizing the production status of various musical instruments.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/Audio-Agenda)
+- [Go To App](https://audio-agenda.streamlit.app/)
+""")
+
+st.subheader("Task Regression")
+st.write("""
+- Developed a streamlit web application that facilitates task management and content organization with the ability to save tasks, links, uploaded files, text entries, and code snippets associated with each task.
+- [GitHub Repository](https://github.com/SimpleMobileResponsiveWebsites/TaskRegression)
+- [Go To Application](https://taskregression-nd9fndntyb22oc97txsbj4.streamlit.app/)
+""")
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.write("For Data Science Application Development:")
+    st.write("Send project inquires to via Email: simplemobileresponsivewebsites@gmail.com")
+    st.write("Send a direct message using LinkedIn: [linkedin.com/in/nathan-rossow-58912238](https://www.linkedin.com/in/nathan-rossow-58912238/)")
